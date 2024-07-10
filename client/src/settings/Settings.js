6d281298316bfb93ab1493cf3a346d6b2b5eefe3
amimul-arnab <arnabamimul92@gmail.com>
2024-07-10 17:35:22 -0400

    Complete UX/UI for settings page

--- client/src/settings/Settings.js
+++ client/src/settings/Settings.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import './Settings.css';
 
-const Settings = () => {
+function Settings() {
   return (
-    <div>
-      <h1>Settings Page</h1>
+    <div className="settings">
+      <main className="settings-main">
+        <div className="profile">
+          <h2>Profile:</h2>
+          <div className="profile-item">
+            <label>Name:</label>
+            <span></span>
+            <button>Change</button>
+          </div>
+          <div className="profile-item">
+            <label>Email:</label>
+            <span></span>
+            <button>Change</button>
+          </div>
+          <div className="profile-item">
+            <label>Password:</label>
+            <span></span>
+            <button>Change</button>
+          </div>
+        </div>
+        <div className="account-management">
+          <h2>Account Management:</h2>
+          <div className="account-management-buttons">
+            <button className="sign-out">Sign Out</button>
+            <button className="delete-account">Permanently Delete User</button>
+          </div>
+        </div>
+      </main>
     </div>
   );
-};
+}
 
 export default Settings;
